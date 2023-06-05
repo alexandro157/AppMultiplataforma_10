da270355234926ecdfb4ef287e6441c01529faba
Alexandro157 <alexandroadrian4@gmail.com>
2023-06-04 23:55:16 -0500

    Agregar archivos de proyecto.

---
+++ Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
